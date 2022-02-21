431273e3d802a4b7b87bc7c8f67123d7111b4a59
NguyenSyTien-LTHDT <tiensizzzzzz@gmailcom>
2022-02-21 12:59:22 +0700

    00

--- OOP_K14MT01/src/baitap/myintegerTest.java
+++ OOP_K14MT01/src/baitap/myintegerTest.java
@@ -2,12 +2,12 @@ package baitap;
 
 public class myintegerTest {
      public static void main(String[] args) { 
-        myinterge interger;
-         interger = new myinterge(3);
+          myInteger interger;
+         interger = new myInteger(3);
          interger.show();
          interger.increment();
          myintegerTest integerone;
-         integerone = new myinterge(1);
+         integerone = new integerone(1);
          System.out.println(integerone.greaterThan(interger));
          System.out.println(integerone.lessThan(interger));
 
