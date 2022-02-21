60381f6aacb95365cbf9b69bb6fef3f889165414
NguyenSyTien-LTHDT <tiensizzzzzz@gmailcom>
2022-02-21 13:05:10 +0700

    00

--- OOP_K14MT01/src/lap4/sanpham.java
+++ OOP_K14MT01/src/lap4/sanpham.java
@@ -1,20 +1,20 @@
 package lap4;
 
 public class sanpham {
-    String tensp;
-    double dongia;
+    String tenSp;
+    double donGia;
     double giamGia;
-    public String tensp(){  
-        return tensp;
+    public String tenSp(){  
+        return tenSp;
     }
     public void setTenSP(String tenSp){
-        this.tensp = tenSp;
+        this.tenSp = tenSp;
     }
     public double setDonGia(){
-        return dongia;
+        return donGia;
     }
-    public void dongia(Double dongia){
-        this.dongia = dongia;
+    public void dongia(Double donGia){
+        this.donGia = donGia;
     }
     public double setGiamGia(){
         return giamGia;
@@ -22,7 +22,29 @@ public class sanpham {
     public void giamGia(Double giamGia){
         this.giamGia = giamGia;
     }
+    public sanpham(String tenSp, Double donGia, Double giamGia){
+        this.tenSp = tenSp;
+        this.donGia = donGia;
+        this.giamGia = giamGia;
+    }
+    
+    private double getThueNhapKhau() {
+        return donGia * 0.1;
+
+
+        
+    }
+    public void xuat() {
+        System.out.println("Ten San Pham:"+ tenSp);
+        System.out.println("DOnGia:"+ donGia);
+        System.out.println("giamGia:"+giamGia);
+        System.out.println("Thue Nhap Khau:"+ getThueNhapKhau());
+        return ;
+        
+    }
+        
+    }
     
        
     
-}
+
